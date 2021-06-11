6c6578cd0f77232ab67d7fe1a1cb42c61a60c8ce
FatemehRamezani78 <fati78rn@gmail.com>
2021-06-11 11:14:42 -0700

    module-info updated #11

--- src/module-info.java
+++ src/module-info.java
@@ -3,6 +3,9 @@ module SchoolSystem {
     requires javafx.fxml;
     requires javafx.controls;
     requires javafx.graphics;
+    requires java.sql;
+    requires sqlite.jdbc;
+
 
     opens testJavaFX;
 }
