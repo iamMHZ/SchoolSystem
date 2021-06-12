64a32c2eef253e9cceb524c62fb1a0dc77f326a9
Mohammad Hossein Ziyaaddini <iammhz77@gmail.com>
2021-06-12 21:05:07 +0430

    #12 Added skeleton of the controller

--- src/login/LoginController.java
+++ src/login/LoginController.java
@@ -1,4 +1,35 @@
 package login;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+
 public class LoginController {
+
+    @FXML
+    private AnchorPane signInPane;
+
+    @FXML
+    private Button signInBtn;
+
+    @FXML
+    private AnchorPane loginPane;
+
+    @FXML
+    private Button loginBtn;
+
+    @FXML
+    private Button changeToSignInBtn;
+
+    @FXML
+    void onLoginBtnClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void onSignInBtnsClicked(MouseEvent event) {
+
+    }
+
 }
