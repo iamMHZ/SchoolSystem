f44a446134b0c660d05acd1506c1036c4d1d25d4
Mohammad Hossein Ziyaaddini <iammhz77@gmail.com>
2021-06-28 22:09:34 +0430

    added Persian fillers to singin pane #12

--- src/login/LoginController.java
+++ src/login/LoginController.java
@@ -3,25 +3,59 @@ package login;
 import animatefx.animation.*;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 
 public class LoginController {
-
-    @FXML
-    private AnchorPane signInPane;
-
-    @FXML
-    private Button signInBtn;
-
     @FXML
     private AnchorPane loginPane;
+
+    @FXML
+    private TextField loginUsername;
+
+    @FXML
+    private PasswordField loginPassword;
 
     @FXML
     private Button loginBtn;
 
     @FXML
     private Button changeToSignInBtn;
+
+    @FXML
+    private AnchorPane signInPane;
+
+    @FXML
+    private TextField name;
+
+    @FXML
+    private TextField nationalId;
+
+    @FXML
+    private PasswordField signInPassword1;
+
+    @FXML
+    private PasswordField signInPassword2;
+
+    @FXML
+    private TextField signInUsername;
+
+    @FXML
+    private TextField schoolName;
+
+    @FXML
+    private TextField courseName;
+
+    @FXML
+    private Button signInBtn;
+
+
+    @FXML
+    void backClicked(MouseEvent event) {
+
+    }
 
     @FXML
     void OnMouseEntered(MouseEvent event) {
